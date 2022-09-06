e05a7f5bf89c4ba8bcef20ed1729f83785c5e9f3
komo1048 <komo1048@naver.com>
2022-09-06 21:03:17 +0900

    firebase를 이용해 사용자 계정 관리

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import AppRouter from "./Router";
+import { authService } from "../fBase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
--- src/fBase.js
+++ src/fBase.js
@@ -6,10 +6,13 @@ import "firebase/compat/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyAFAGfn_P-KnUM0kFjOGpO_frqpeAb8Mqc",
   authDomain: "nwitter-84d14.firebaseapp.com",
+  databaseURL: "https://nwitter-84d14-default-rtdb.firebaseio.com/",
   projectId: "nwitter-84d14",
   storageBucket: "nwitter-84d14.appspot.com",
   messagingSenderId: "1055314571616",
   appId: "1:1055314571616:web:e18c1b95a14f590c26ee05",
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
